64cab8249a549695bdb9055cbefb4f1b4f327fb4
Bryce Perez <bryceperez31@gmail.com>
2022-01-12 17:48:08 -0700

    added the Home icon and experimented with different ways to get the appropriate nav button to appear

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 import "./styles.scss";
 
 export default function Header() {
@@ -8,12 +9,22 @@ export default function Header() {
   return (
     <header>
       <h1>Make A Meme</h1>
-      {isHome ? (
-        <Link className="nav-item" to="/" onClick={() => setIsHome(false)}>
-          <h3>Home</h3>
+      {!isHome ? (
+        <Link
+          className="nav-item"
+          to="/"
+          onClick={(prevState) => setIsHome(prevState)}
+        >
+          <h3>
+            <HomeIcon fontSize="large" />
+          </h3>
         </Link>
       ) : (
-        <Link className="nav-item" to="/memes" onClick={() => setIsHome(true)}>
+        <Link
+          className="nav-item"
+          to="/memes"
+          onClick={(prevState) => setIsHome(!prevState)}
+        >
           <h3>Memes</h3>
         </Link>
       )}
